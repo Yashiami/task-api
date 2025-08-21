952b52a4eb403f84941d1c28b92118bb10719cb1
Yashiami <114471786+Yashiami@users.noreply.github.com>
2025-08-21 14:29:02 +0300

    Write and test method for creating items

--- main.go
+++ main.go
@@ -1,4 +1,15 @@
-﻿package main
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"task-api/api_cmd"
+	"task-api/database"
+)
 
 func main() {
+	database.InitDB()
+	router := gin.Default()
+	router.POST("/create", api_cmd.CreateItem)
+	router.Run("localhost:8080")
+
 }
